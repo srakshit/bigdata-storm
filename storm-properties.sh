d958b84f6468bc27f52530f8afdc2202f60639ea
Subham Rakshit <developersubham@gmail.com>
2017-03-09 23:37:09 +0000

    Paramaterised zookeeper and nimbus config

--- storm-properties.sh
+++ storm-properties.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-export IP=`hostname -i`
-sed -i -e "s/%zookeeper%/zookeeper/g" $STORM_HOME/conf/storm.yaml
-sed -i -e "s/%nimbus%/$IP/g" $STORM_HOME/conf/storm.yaml
+sed -i -e "s/%zookeeper%/$ZK_ADDR/g" $STORM_HOME/conf/storm.yaml
+sed -i -e "s/%nimbus%/$NIMBUS_ADDR/g" $STORM_HOME/conf/storm.yaml
 
 echo "storm.local.hostname: `hostname -i`" >> $STORM_HOME/conf/storm.yaml
